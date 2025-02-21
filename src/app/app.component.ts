68dbb99c843b615cf16e3816128af6304e3ba379
bipulsardar421 <bipulsardar421@gmail.com>
2025-02-21 10:26:15 +0530

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, TemplateRef } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { MainComponent } from "./component/main/main.component";
 import { ModalService } from './component/data/services/modal.service';
 import { LoginComponent } from "./component/common/pop-up-component/login/login.component";
@@ -9,6 +9,7 @@ import { ForgotPwdComponent } from "./component/common/pop-up-component/forgot-p
 import { ResetPwdComponent } from "./component/common/pop-up-component/reset-pwd/reset-pwd.component";
 import { HomeComponent } from "./component/home/home.component";
 import { NgModel } from '@angular/forms';
+import AOS from 'aos';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, CommonModule],
@@ -22,6 +23,28 @@ export class AppComponent {
 
   openModal(content: TemplateRef<any>) {
     this.modalService.open(content);
+  }
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          AOS.refresh(); // Refresh AOS on route change
+        }, 300);
+      }
+    });
+  }
+
+  ngOnInit() {
+    AOS.init({
+      offset: 120,
+      delay: 0,
+      duration: 900,
+      easing: "ease",
+      once: false,
+      mirror: false,
+      anchorPlacement: "top-bottom",
+    });
   }
 
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { MainComponent } from './component/main/main.component';
 import { HomeComponent } from './component/home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
     { path: 'main', component: MainComponent },
-    { path: '**', redirectTo: '' }
+    { path: '**', redirectTo: 'home' }
 ];
