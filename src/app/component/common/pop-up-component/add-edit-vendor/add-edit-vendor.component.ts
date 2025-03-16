2d4d4324ef6475130ec95e0fe8c9e31e85514543
bipulsardar104 <64055588+bipulsardar421@users.noreply.github.com>
2025-03-16 14:42:12 +0530

    .

--- src/app/component/common/pop-up-component/add-edit-vendor/add-edit-vendor.component.ts
+++ src/app/component/common/pop-up-component/add-edit-vendor/add-edit-vendor.component.ts
@@ -1,13 +1,4 @@
-import {
-  Component,
-  inject,
-  TemplateRef,
-  ViewChild,
-  OnInit,
-  Inject,
-  signal,
-  DestroyRef,
-} from '@angular/core';
+import { Component, inject, TemplateRef, ViewChild } from '@angular/core';
 import {
   FormArray,
   FormBuilder,
@@ -21,85 +12,70 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 import { UsersInterface } from '../../../data/interfaces/users.interface';
 import { LoginService } from '../../../data/services/login/login.service';
+import { FormDataConverter } from '../../../data/helper/formdata.helper';
 import { AlertService } from '../../../data/services/alert.service';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-add-edit-vendor',
-  standalone: true,
   imports: [ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './add-edit-vendor.component.html',
   styleUrl: './add-edit-vendor.component.css',
 })
-export class AddEditVendorComponent implements OnInit {
+export class AddEditVendorComponent {
   @ViewChild('addEditVendor') addEditVendor!: TemplateRef<any>;
-
-  private userId = localStorage.getItem('user_id');
-  private destroyRef = inject(DestroyRef);
   private modalService = inject(ModalService);
   private ngbModalService = inject(NgbModal);
-  private fb = inject(FormBuilder);
-  private loginService = inject(LoginService);
-  private alertService = inject(AlertService);
-
-  what: string = 'Add';
+  what: String = 'Add';
   dto!: UsersInterface;
   userForm!: FormGroup;
   users: any[] = [];
   initialState: any[] = [];
-  users$!: Observable<any[]>;
-  constructor() {
+
+  constructor(
+    private fb: FormBuilder,
+    private _login: LoginService,
+    private _alert: AlertService
+  ) {
     this.userForm = this.fb.group({
       users: this.fb.array([]),
     });
   }
 
   ngOnInit() {
-    this.modalService.openModalEvent
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((modalType: ModalType) => {
-        if (modalType === ModalType.VENDOR) {
-          this.handleModalOpen();
+    this.modalService.openModalEvent.subscribe((modalType: ModalType) => {
+      if (modalType === ModalType.VENDOR) {
+        this.openModal();
+      }
+    });
+    this.modalService.dataTransferObject.subscribe((dto: any) => {
+      if (dto != null) {
+        this.what = dto.type;
+        if (dto.type === 'edit') {
+          this.dto = dto.data;
         }
-      });
-
-    this.modalService.dataTransferObject
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((dto: any) => {
-        if (dto) {
-          this.what = dto.type;
-          if (dto.type === 'edit') {
-            this.dto = dto.data;
-          }
-        }
-      });
-
+      }
+    });
     this.fetchUsers();
   }
-
   fetchUsers(): void {
-    if (!this.userId) {
+    const fd = new FormData();
+    this._alert.showLoading();
+    const userId = localStorage.getItem('user_id');
+    if (userId) {
+      fd.append('id', userId);
+    } else {
       console.error('User ID not found in localStorage');
-      return;
     }
-
-    const formData = new FormData();
-    formData.append('id', this.userId);
-
-    this.alertService.showLoading();
-    this.users$ = this.loginService.getUserDetails(formData);
-
-    this.users$.pipe(takeUntilDestroyed(this.destroyRef)).subscribe({
+    this._login.getUserDetails(fd).subscribe({
       next: (apiResponse) => {
-        this.alertService.hideLoading();
+        this._alert.hideLoading();
         this.users = this.processUsers(apiResponse);
         this.initializeForm();
         this.storeInitialState();
       },
-      error: () => {
-        this.alertService.hideLoading();
-        this.alertService.showAlert(
+      error: (err) => {
+        this._alert.hideLoading();
+        this._alert.showAlert(
           'danger',
           'Something Went Wrong, please try again!!'
         );
@@ -112,21 +88,37 @@ export class AddEditVendorComponent implements OnInit {
       id: user.id,
       name: user.user_name,
       email: user.username,
-      role: user.role,
+      isClient: user.role === 'client',
+      isVendor: user.role === 'vendor',
+      isAdmin: user.role === 'admin',
     }));
   }
 
   initializeForm(): void {
-    const userControls = this.users.map((user) =>
+    const userControls = this.users.map((user: any) =>
       this.fb.group({
         id: [user.id],
         name: [user.name],
         email: [user.email],
-        role: [user.role, Validators.required],
+        isClient: [user.isClient],
+        isVendor: [user.isVendor],
+        isAdmin: [user.isAdmin],
       })
     );
 
     this.userForm.setControl('users', this.fb.array(userControls));
+  }
+
+  toggleRole(userIndex: number, selectedRole: string) {
+    const userFormGroup = (this.userForm.get('users') as FormArray).at(
+      userIndex
+    );
+
+    userFormGroup.patchValue({
+      isClient: selectedRole === 'client',
+      isVendor: selectedRole === 'vendor',
+      isAdmin: selectedRole === 'admin',
+    });
   }
 
   storeInitialState(): void {
@@ -134,31 +126,57 @@ export class AddEditVendorComponent implements OnInit {
   }
 
   submitRoles(): void {
-    this.alertService.showLoading();
+    this._alert.showLoading();
+    const updatedUsers = this.userForm.value.users
+      .filter(
+        (user: any, index: number) =>
+          JSON.stringify(user) !== JSON.stringify(this.initialState[index])
+      )
+      .flatMap((user: any) => [
+        user.isClient
+          ? {
+              role: 'client',
+              user_name: user.name,
+              id: user.id,
+              username: user.email,
+            }
+          : null,
+        user.isVendor
+          ? {
+              role: 'vendor',
+              user_name: user.name,
+              id: user.id,
+              username: user.email,
+            }
+          : null,
+        user.isAdmin
+          ? {
+              role: 'admin',
+              user_name: user.name,
+              id: user.id,
+              username: user.email,
+            }
+          : null,
+      ])
+      .filter((role: any) => role !== null);
 
-    const updatedUsers = this.userForm.value.users.filter(
-      (user: any, index: number) =>
-        JSON.stringify(user) !== JSON.stringify(this.initialState[index])
-    );
-
-    if (updatedUsers.length === 0) {
-      this.alertService.hideLoading();
-      this.alertService.showAlert('info', 'No changes detected.');
-      return;
-    }
+    console.log(updatedUsers);
 
     const formData = new FormData();
+
     formData.append('data', JSON.stringify(updatedUsers));
 
-    this.loginService.editUserRole(formData).subscribe({
-      next: () => {
-        this.alertService.hideLoading();
-        this.alertService.showAlert('success', 'Roles Updated!');
+    this._login.editUserRole(formData).subscribe({
+      next: (res) => {
+        this._alert.hideLoading();
+        this._alert.showAlert('success', 'Role Updated!');
+        this.users = [];
+        this.initialState = [];
         this.fetchUsers();
       },
-      error: () => {
-        this.alertService.hideLoading();
-        this.alertService.showAlert(
+      error: (err) => {
+        this._alert.hideLoading();
+        this._alert.showAlert(
           'danger',
           'Something went wrong, please try again!'
         );
@@ -166,13 +184,8 @@ export class AddEditVendorComponent implements OnInit {
     });
   }
 
-  get usersControls() {
+  getUsersControls() {
     return (this.userForm.get('users') as FormArray).controls;
-  }
-  private handleModalOpen() {
-    if (!this.ngbModalService.hasOpenModals()) {
-      this.openModal();
-    }
   }
   openModal() {
     if (this.addEditVendor) {
